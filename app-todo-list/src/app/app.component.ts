34f59942428e1f40d98ef01e91ce70fbafd4b86f
unknown <Ruan Pissinati>
2024-10-12 04:41:50 -0300

    feat: Mostrando utilização dos parametros templateUrl e styleUrl, dessa forma voce consegue separar os arquivos da aplicação

--- app-todo-list/src/app/app.component.ts
+++ app-todo-list/src/app/app.component.ts
@@ -3,14 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
-  template:`
-    <li>Item da lista</li>
-  `,
-  styles:`
-    li {
-      color: red
-    }
-  `
+  templateUrl:'./app.component.html',
+  styleUrl:'./app.component.css'
 })
 export class AppComponent {
 }
